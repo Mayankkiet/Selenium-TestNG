91811395ac8dd4e6888386d5767a0b4127c1e89f
Mayank <Mayank@Mayank-PC>
2020-04-28 23:47:43 +0530

    First Run

--- TestNG Learning/src/demo/loginTest.java
+++ TestNG Learning/src/demo/loginTest.java
@@ -1,19 +1,45 @@
 package demo;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class loginTest {
-	
+
 	WebDriver driver;
 	String url = "http://the-internet.herokuapp.com/";
-	
+
 	@BeforeClass
-	public void setup()
-{
-	System.setProperty("webdriver.chrome.driver","D:\\Selenium JARS and Drivers\\chromedriver.exe");
-	driver = new ChromeDriver();
-	driver.get(url);
-}
+	public void setup() {
+		// Set the key/value property according to the browser you are using.
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium JARS and Drivers\\chromedriver.exe");
+
+		// Open browser instance
+		driver = new ChromeDriver();
+
+		// Open AUT
+		driver.get(url);
+	}
+
+	@Test
+	public void test() {
+		String pageTitle = driver.getTitle();
+		System.out.println(pageTitle);
+		WebElement loginUrl = driver.findElement(By.linkText("Form Authentication"));
+		loginUrl.click();
+		driver.findElement(By.xpath("//input[@name ='username']")).sendKeys("tomsmith");
+		driver.findElement(By.xpath("//input[@name ='password']")).sendKeys("SuperSecretPassword!");
+		driver.findElement(By.xpath("//button[@class ='radius']")).click();
+	}
+
+	@AfterClass
+	public void teardown() {
+		// Close the browser
+		driver.close();
+
+	}
 }
