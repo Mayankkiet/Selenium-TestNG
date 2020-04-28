549676bb656b28e0086c12c197c1a50f40fdcb2b
Mayank <Mayank@Mayank-PC>
2020-04-28 23:27:14 +0530

    commit

--- TestNG Learning/src/demo/loginTest.java
+++ TestNG Learning/src/demo/loginTest.java
@@ -1,12 +1,19 @@
 package demo;
 
 import org.openqa.selenium.WebDriver;
-
-
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
 
 public class loginTest {
 	
 	WebDriver driver;
+	String url = "http://the-internet.herokuapp.com/";
+	
+	@BeforeClass
+	public void setup()
+{
 	System.setProperty("webdriver.chrome.driver","D:\\Selenium JARS and Drivers\\chromedriver.exe");
 	driver = new ChromeDriver();
+	driver.get(url);
+}
 }
